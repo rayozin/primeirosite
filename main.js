835cf8dbdeca6923172d90710592cb30918a0e41
rayozin <142932535+rayozin@users.noreply.github.com>
2023-09-21 16:15:32 -0300

    osmantuswine

---
+++ main.js
@@ -0,0 +1,17 @@
+function voltarTema(){
+    document.getElementById("fundo").style.background="crimson";
+    document.getElementById("fundo").style.color="black";
+}
+
+function mudarTema(){
+    document.getElementById("fundo").style.background="black";
+    document.getElementById("fundo").style.color="gold";
+}
+
+function mouseForadaImagem(){
+    document.getElementById("trio").src="2348317.jpg";
+}
+
+function mouseDentrodaImagem(){
+    document.getElementById("trio").src="img3.jpg";
+}
